92b903fb5583e3e362335a801bed1c34f9a05502
James Reilly <jreilly2112@gmail.com>
2022-03-18 20:21:53 -0400

    stole a 404 page

--- Assets/server.js
+++ Assets/server.js
@@ -19,7 +19,11 @@ app.get('/', (req, res) =>
 
 // GET Route for feedback page
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/notes.html'))
+  res.sendFile(path.join(__dirname, './public/pages/notes.html'))
+);
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/pages/404.html'))
 );
 
 app.listen(PORT, () =>
